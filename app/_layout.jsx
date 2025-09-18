92910fbe3b9b725db6ab27709ab7da0771bef283
Ivan Filipović <filipovicivan8@gmail.com>
2025-09-18 17:17:10 +0200

    Created database and table for storing books. Added page for users to be able to create new book

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,30 +1,32 @@
 import { Stack } from "expo-router";
-import { StyleSheet, Text, useColorScheme, View } from "react-native";
 import { Colors } from "../constants/Colors";
+import { useColorScheme } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { UserProvider } from "../contexts/UserContext";
+import { BooksProvider } from "../contexts/BooksContext";
 
-const RootLayout = () => {
+export default function RootLayout() {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
 
   return (
     <UserProvider>
-      <StatusBar value="auto" />
-      <Stack
-        screenOptions={{
-          headerStyle: { backgroundColor: theme.navBackground },
-          headerTintColor: theme.title,
-        }}
-      >
-        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-        <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
-        <Stack.Screen name="index" options={{ title: "Home" }} />
-      </Stack>
+      <BooksProvider>
+        <StatusBar value="auto" />
+        <Stack
+          screenOptions={{
+            headerStyle: { backgroundColor: theme.navBackground },
+            headerTintColor: theme.title,
+          }}
+        >
+          {/* Groups */}
+          <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+          <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
+
+          {/* Individual Screens */}
+          <Stack.Screen name="index" options={{ title: "Home" }} />
+        </Stack>
+      </BooksProvider>
     </UserProvider>
   );
-};
-
-export default RootLayout;
-
-const styles = StyleSheet.create({});
+}
